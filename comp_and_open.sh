4ca0220cb954d348a6acb3f97bed8a050c837377
Aaron Block <profblock@users.noreply.github.com>
2018-12-19 15:47:26 -0600

    Fixed comp and open

--- comp_and_open.sh
+++ comp_and_open.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-if [ $? = 0 ] ; then
-	if [ "$#" -gt "0" ]; then 
-		pdflatex -halt-on-error "$1.tex"
-		if [ $? != 0 ] ; then #$? gets error code from last command
-		  echo "LateXing failed!"
-		else 
-		 echo "LaTeXing worked"
-		fi
-	else
-		echo "Insufficient paramaters"
+if [ "$#" -gt "0" ]; then 
+	pdflatex -halt-on-error "$1.tex"
+	if [ $? != 0 ] ; then #$? gets error code from last command
+		echo "LateXing failed!"
 	fi
-else 
-	echo "pull failed"
+else
+	echo "Insufficient paramaters"
 fi
