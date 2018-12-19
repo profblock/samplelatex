e2bcc683fe4899f5ef610420c98e32f0e2e2bb29
Aaron Block <profblock@users.noreply.github.com>
2018-12-19 15:44:26 -0600

    updated comp and shortened it

--- comp_and_open.sh
+++ comp_and_open.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
-#git pull
 if [ $? = 0 ] ; then
 	if [ "$#" -gt "0" ]; then 
-		TEX_NAME="$1.tex"
-		echo "$TEX_NAME"
-		pdflatex -halt-on-error "$TEX_NAME"
-		if [ $? = 0 ] ; then #$? gets error code from last command
-#		  osascript -e 'quit app "Preview"'
-		  echo "Compilation worked!"
-#		  PDF_NAME="$1.pdf"
-#		  open "/Applications/Preview.app/" "$PDF_NAME"
-#		  git commit -a --allow-empty-message -m ''
-#		  if [ $? = 0 ] ; then
-#			  git push 
-#		  fi
+		pdflatex -halt-on-error "$1.tex"
+		if [ $? != 0 ] ; then #$? gets error code from last command
+		  echo "LateXing failed!"
 		else 
-		 echo "LaTeXing failed"  
+		 echo "LaTeXing worked  
 		fi
 	else
 		echo "Insufficient paramaters"
@@ -23,4 +13,3 @@ if [ $? = 0 ] ; then
 else 
 	echo "pull failed"
 fi
-exit
